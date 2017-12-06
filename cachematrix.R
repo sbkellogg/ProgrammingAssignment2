b6b020a1314abebce652fad01f004d0c2170236e
sbkellogg <sbkellog@ncsu.edu>
2017-12-06 12:36:58 -0500

    R Programming Assignment 2
    The following pair of R functions address the costly computation of matrix inversion by caching the inverse of a matrix rather than computing it repeatedly.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following pair of R functions address the costly computation of matrix inversion by caching the inverse of a matrix rather than computing it repeatedly.
 
-## Write a short comment describing this function
+## The first function is a slight tweak to the example function that creates a special "matrix" object that can cache its inverse. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set_matrix <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get_matrix <- function() x
+        set_inverse <- function(inverse) m <<- inverse
+        get_inverse <- function() m
+        list(set_matrix = set_matrix, get_matrix = get_matrix,
+             set_inverse = set_inverse,
+             get_inverse = get_inverse)
 }
 
 
-## Write a short comment describing this function
+## The second function computes the inverse of a square matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$get_inverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get_matrix()
+        m <- solve(data, ...)
+        x$set_inverse(m)
+        m
 }
